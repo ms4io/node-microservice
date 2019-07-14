1edb259d06ea3608545d4850f02d572076bb2ba0
root <root@devel.ms4.io>
2019-07-14 19:20:35 +0000

    alterei o deploy para remover o anterio

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.status(200).send('GABRIEL HARTMANN LONTRA');
+    res.status(200).send('RELATIVES ------ GABRIEL HARTMANN LONTRA');
 });
 
 var server = app.listen(3000, function () {
