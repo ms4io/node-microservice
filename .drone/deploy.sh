5eaa727f1a1a6781f1b4c1e3f8cd9c204b4b8e7a
root <root@devel.ms4.io>
2019-07-14 18:42:51 +0000

    docker rmi ms4io commented

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 # docker rm -f ms4io_node_micro
-docker rmi ms4io/node-microservice
+# docker rmi ms4io/node-microservice
 docker pull ms4io/node-microservice
 docker run --rm -d -p 7777:3000 --name ms4io_node_micro ms4io/node-microservice
